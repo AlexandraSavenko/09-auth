540ece711560f4ceacd8033a32713ef4bb86532a
Sasha Savenko <sashasavenko3@gmail.com>
2025-10-17 20:37:42 +0300

    fetchNotes changed

--- lib/api.ts
+++ lib/api.ts
@@ -21,19 +21,23 @@ interface FetchNoteDetails {
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
+    withCredentials: true,
     headers: {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`
     }
 
 })
 export const fetchNotes = async ({category, searchValue, page}: FetchNotesParams): Promise<FetchNotesResponse> => {
-    const searchParams: Record<string, string> = {}
-    if(searchValue) searchParams.search = searchValue
-    if(page) searchParams.page = page.toString()
-        if(category) searchParams.tag = category
-    const query = new URLSearchParams(searchParams)
-    console.log(process.env.NEXT_PUBLIC_API_URL)
-const res = await api.get<FetchNotesResponse>(`/notes?${query}`);
+//     const searchParams: Record<string, string> = {}
+//     if(searchValue) searchParams.search = searchValue
+//     if(page) searchParams.page = page.toString()
+//         if(category) searchParams.tag = category
+//     const query = new URLSearchParams(searchParams)
+//     console.log(process.env.NEXT_PUBLIC_API_URL)
+// const res = await api.get<FetchNotesResponse>(`/notes?${query}`);
+const res = await api.get<FetchNotesResponse>('/notes', {
+    params: { category, searchValue, page }
+})
 return res.data
 }
 
